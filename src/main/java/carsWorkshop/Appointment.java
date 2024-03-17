b1b3269ef9cc88db69527997d930aff494adac55
ValPerSeb <sebasvallejoperez@hotmail.com>
2024-03-17 02:24:00 -0500

    added validations and methods for classes Customer, Appointment and CustomerManagement

--- src/main/java/carsWorkshop/Appointment.java
+++ src/main/java/carsWorkshop/Appointment.java
@@ -1,9 +1,40 @@
 package carsWorkshop;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
 public class Appointment {
     int consecutiveId;
-    String date;
-    String customerId;
+    LocalDateTime date;
+    int customerId;
     String vehicleLicense;
     String type;
+    
+    public Appointment(int consecutiveId, LocalDateTime date, int customerId, String vehicleLicense, String type){
+        String newConsecutiveId = String.valueOf(consecutiveId);
+        if(newConsecutiveId.length() > 6){
+            System.out.println("El consecutivo no puede ser mayor a 6 dígitos");
+        }else{
+            this.consecutiveId = consecutiveId;
+        }
+        
+        String newType = type.toLowerCase();
+        if(!"reparación".equals(newType) && !"mantenimiento".equals(newType)){
+            System.out.println("El tipo ingresado no es válido");
+        }else{
+            this.type = newType;
+        }
+                
+        int day = date.getDayOfWeek().getValue();
+        String[] holidays = {"2024-03-25", "2024-03-28", "2024-03-29", "2024-05-01", "2024-05-13", "2024-06-03", "2024-06-10", "2024-07-01", "2024-08-07", "2024-08-19", "2024-10-14", "2024-11-04", "2024-11-11", "2024-12-25"};
+        if(day == 6 || day == 7){
+            System.out.println("El taller no acepta citas fines de semana");
+        //}else if(Arrays.binarySearch(holidays, date.)){
+        }else{
+            this.date = date;
+        }
+       
+        this.customerId = customerId;
+        this.vehicleLicense = vehicleLicense;
+    }
 }
--- src/main/java/carsWorkshop/CustomerManagement.java
+++ src/main/java/carsWorkshop/CustomerManagement.java
@@ -2,7 +2,50 @@ package carsWorkshop;
 import java.util.ArrayList;
         
 public class CustomerManagement {
-    ArrayList  activeCustomers;
-    ArrayList inProgressCustomers;
-    ArrayList readyCustomers;
+    ArrayList<Customer> activeCustomers;
+    ArrayList<Customer> inProgressCustomers;
+    ArrayList<Customer> readyCustomers;
+    
+    /*Métodos de activeCustomers*/
+    public void RegisterCustomer(Customer newCustomer){
+        this.activeCustomers.add(newCustomer);
+    }
+    
+    public void PrintActiveCustomers(){
+        System.out.println("Los siguientes clientes se encuentran activos: ");
+        for(Customer customer : this.activeCustomers){
+            System.out.println(" > " + customer.name);
+        }
+    }
+    
+    /*Métodos de inProgressCustomers*/
+    public void RegisterInProgressCustomer(int customerId){
+        for(Customer customer : this.activeCustomers){
+            if(customer.id == customerId){
+                this.inProgressCustomers.add(customer);
+            }
+        }
+    }
+    public void PrintInProgressCustomers(){
+        System.out.println("Los siguientes clientes se encuentran en reparación o mantenimiento: ");
+        for(Customer customer : this.inProgressCustomers){
+            System.out.println(" > " + customer.name);
+        }
+    }
+    
+    /*Métodos de readyCustomers*/
+    public void RegisterReadyCustomer(int customerId){
+        for(Customer customer : this.activeCustomers){
+            if(customer.id == customerId){
+                this.readyCustomers.add(customer);
+            }
+        }
+    }
+    
+    public void PrintReadyCustomers(){
+        System.out.println("Los siguientes clientes se encuentran listos: ");
+        for(Customer customer : this.readyCustomers){
+            System.out.println(" > " + customer.name);
+        }
+    }
 }
