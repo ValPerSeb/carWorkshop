06bbb4290a8176cf2e16c283a11dd5bfa2027b56
lorenagomez2112 <161979893+lorenagomez2112@users.noreply.github.com>
2024-04-01 13:51:01 -0500

    Update CarsWorkshop.java

--- src/main/java/carsWorkshop/CarsWorkshop.java
+++ src/main/java/carsWorkshop/CarsWorkshop.java
@@ -29,7 +29,12 @@ public class CarsWorkshop {
         Customer newCustomer = new Customer(id, name, lastName, address, phone);
         //Inicio validaciones para evitar campos vacios o con error.
         if(newCustomer.id == 0){
-            newCustomer.id = Long.parseLong(JOptionPane.showInputDialog("Ingrese el documento de identidad del cliente: "));
+            String vali="123456789012";
+            do{
+                System.out.println("El id no puede ser mayor a 11 dígitos");
+                newCustomer.id = Long.parseLong(JOptionPane.showInputDialog("Ingrese el documento de identidad del cliente: "));
+                vali=String.valueOf(newCustomer.id);
+            }while(vali.length()>11);
         }
         if(newCustomer.name == null){
             newCustomer.name = JOptionPane.showInputDialog("Ingrese el nombre del cliente: ");
@@ -41,21 +46,58 @@ public class CarsWorkshop {
             newCustomer.address = JOptionPane.showInputDialog("Ingrese la dirección del cliente: ");
         }
         if(newCustomer.phone == 0){
-            newCustomer.phone = Long.parseLong(JOptionPane.showInputDialog("Ingrese el teléfono del cliente: "));
+            String vali="123456789012";
+            do{
+                System.out.println("El id no puede ser mayor a 11 dígitos");
+                newCustomer.phone = Long.parseLong(JOptionPane.showInputDialog("Ingrese el documento de identidad del cliente: "));
+                vali=String.valueOf(newCustomer.phone);
+            }while(vali.length()>11);
         }
         //Fin validaciones para evitar campos vacios o con error.
         customerMgmt.RegisterCustomer(newCustomer);
         
         System.out.println("**Registro de Nuevo Vehiculo**");
         String licensePlate = JOptionPane.showInputDialog("Ingrese la placa del vehiculo: ");
+        if(licensePlate.length()>6){
+            do{
+                licensePlate = JOptionPane.showInputDialog("Recuerda que placa debe ser menor a 6 caracteres,  Ingrese la placa del vehiculo: ");
+            }while(licensePlate.length()>6);
+        }
+        if(licensePlate.length()<=0){
+            do{
+                licensePlate = JOptionPane.showInputDialog("No has puesto ninguna placa,Ingrese la placa del vehiculo: "); 
+            }while(licensePlate.length()<=0);
+        }
+        
         String brand = JOptionPane.showInputDialog("Ingrese la marca del vehiculo: ");
+        if (brand.length()<=0){
+            do{
+                brand = JOptionPane.showInputDialog("No has puesto ninguna marca, Ingrese la marca del vehiculo: ");
+            }while(brand.length()<=0);
+        }
         int model = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el modelo del vehiculo: "));
+        if((String.valueOf(model).length())<=0){
+            String vali="";
+            do{
+                model = Integer.parseInt(JOptionPane.showInputDialog("No has puesto modelo ,Ingrese el modelo del vehiculo: "));
+                vali=String.valueOf(model);
+            }while(vali.length()>=0);
+        }
         double km = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el kilometraje del vehiculo: "));
         String category = JOptionPane.showInputDialog("Ingrese cual es la categoria del vehiculo:"
                    + "Sedan"
                    + "Camioneta"
                    + "Pick up"
                    + "SUV");
+        if(category.length()<=0){
+            do{
+                category = JOptionPane.showInputDialog("Ingrese cual es la categoria del vehiculo:"
+                   + "Sedan"
+                   + "Camioneta"
+                   + "Pick up"
+                   + "SUV");
+            }while(category.length()<=0);
+        }
         
         Vehicle newVehicle = new Vehicle(licensePlate, brand, model, km, category);
         
@@ -71,6 +113,29 @@ public class CarsWorkshop {
         
         apptMgmt.scheduleAppointment(newAppointment);
         
+      
+        
+        int cant= Integer.parseInt(JOptionPane.showInputDialog("cuantos respuestos va a agregar"));
+        Inventory SpareParts[] = new Inventory[cant];
+    
+        for (int i = 0; i < cant; i++) {
+            int itemId=Integer.parseInt(JOptionPane.showInputDialog("ingrese el id"));
+            String itemName=JOptionPane.showInputDialog("ingrese el nombre del repuesto");
+            double unitPrice=Double.parseDouble(JOptionPane.showInputDialog("ingrese el precio de la unidad"));
+            int quantity=Integer.parseInt(JOptionPane.showInputDialog("ingrese la cantidad"));
+                
+                 SpareParts[i]=new Inventory(itemId,itemName,unitPrice,quantity);
+                 
+
+        }
+        for (int i = 0; i < SpareParts.length; i++) {
+            System.out.println("***********************");
+            System.out.println("el id del elemento es :" + SpareParts[i].itemId);
+            System.out.println("el nombre del elemento es :" + SpareParts[i].itemName);
+            System.out.println("el Precio unitario del elemento es :" + SpareParts[i].unitPrice);
+            System.out.println("la cantidad del elemento es :" + SpareParts[i].quantity);
+            System.out.println("***********************");
+        }
         
         
     }
@@ -96,4 +161,6 @@ public class CarsWorkshop {
         
         return consecutiveId;
     }
+    
+    
 }
