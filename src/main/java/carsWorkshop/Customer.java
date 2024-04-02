c41113c6ddbb62db4f017a0c7941675a2466c217
lorenagomez2112 <161979893+lorenagomez2112@users.noreply.github.com>
2024-04-01 20:10:50 -0500

    Update Customer.java

--- src/main/java/carsWorkshop/Customer.java
+++ src/main/java/carsWorkshop/Customer.java
@@ -1,12 +1,12 @@
 package carsWorkshop;
 
 public class Customer {
-    int id;
+    long id; //Se utiliza long debido a la cantidad de caracteres.
     String name;
     String lastName;
     String address;
-    int phone;
-    public Customer(int id, String name, String lastName, String address, int phone){
+    long phone; //Se utiliza long debido a la cantidad de caracteres.
+    public Customer(long id, String name, String lastName, String address, long phone){
         String newId = String.valueOf(id);
         if(newId.length() > 11){
             System.out.println("El id no puede ser mayor a 11 dígitos");
@@ -14,6 +14,18 @@ public class Customer {
             this.id = id;
         }
         
+        if(name.length() <= 0){
+            System.out.println("El nombre no puede estar vacío");
+        }else{
+            this.name = name;
+        }
+        
+        if(lastName.length() <= 0){
+            System.out.println("El apellido no puede estar vacío");
+        }else{
+            this.lastName = lastName; 
+        }
+            
         if(address.length() > 30){
             System.out.println("La dirección no puede ser mayor a 30 dígitos");
         }else{
@@ -25,9 +37,6 @@ public class Customer {
             System.out.println("El teléfono no puede ser mayor a 11 dígitos");
         }else{
             this.phone = phone;
-        }
-        
-        this.name = name;
-        this.lastName = lastName;      
+        }  
     }
 }
